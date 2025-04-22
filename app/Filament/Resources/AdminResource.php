1bb358f0830c985824077b65e1558e750b2be858
dimasdaffa <dimas.de2004@gmail.com>
2025-04-22 10:24:20 +0700

    admin form

--- app/Filament/Resources/AdminResource.php
+++ app/Filament/Resources/AdminResource.php
@@ -12,18 +12,42 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 
 class AdminResource extends Resource
 {
     protected static ?string $model = Admin::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-circle';
+
+    protected static ?string $navigationLabel = 'Admin';
+
+    protected static ?string $modelLabel = 'Admin';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('username')
+                    ->required()
+                    ->maxLength(50)
+                    ->unique(ignoreRecord: true)
+                    ->label('Username'),
+
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(100)
+                    ->unique(ignoreRecord: true)
+                    ->label('Email'),
+
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(100)
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->label('Password'),
             ]);
     }
 
@@ -31,13 +55,27 @@ class AdminResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('username')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Username'),
+
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Email'),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->label('Dibuat Pada'),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -57,8 +95,8 @@ class AdminResource extends Resource
     {
         return [
             'index' => Pages\ListAdmins::route('/'),
-            'create' => Pages\CreateAdmin::route('/create'),
-            'edit' => Pages\EditAdmin::route('/{record}/edit'),
+            // 'create' => Pages\CreateAdmin::route('/create'),
+            // 'edit' => Pages\EditAdmin::route('/{record}/edit'),
         ];
     }
 }
--- app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
+++ app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAdmin extends CreateRecord
 {
     protected static string $resource = AdminResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
